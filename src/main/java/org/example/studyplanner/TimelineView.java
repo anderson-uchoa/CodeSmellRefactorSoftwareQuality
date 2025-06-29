f2cdcd52d85b3cd38da630b0ad460c341e076774
Nelson <nelsonfelipeandradearaujo@gmail.com>
2025-06-29 15:35:18 -0300

    fix: refactor code smell feature envy using move method refactoring technique

--- src/main/java/org/example/studyplanner/TimelineView.java
+++ src/main/java/org/example/studyplanner/TimelineView.java
@@ -4,22 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class TimelineView {
-
     public String habitDateViewAll(HabitTracker ht){
-        List<Habit> habits = ht.getHabits();
-        StringBuilder response = new StringBuilder();
-        for(Habit habit : habits){
-            response.append("[ Habit: ")
-                    .append(habit.getName())
-                    .append(". Records: ");
-            List<LocalDateTime> records = ht.getHabitRecords(habit.getId());
-            for(LocalDateTime record : records){
-                response.append(ht.formatHabitDate(record)).append(", ");
-            }
-            response.append("]");
-        }
-
-        return response.toString();
+        return ht.getHabitsTimelineView();
     }
-
 }
