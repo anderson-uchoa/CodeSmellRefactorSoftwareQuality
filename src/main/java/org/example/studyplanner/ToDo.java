dee962f8cdf3c2ab889493e826e3ab7cb8c15fd6
Suziane Brandão <suziane90@alu.ufc.br>
2025-07-29 17:15:42 -0300

    renomear: Data Class Todo

--- src/main/java/org/example/studyplanner/ToDo.java
+++ src/main/java/org/example/studyplanner/ToDo.java
@@ -2,7 +2,7 @@ package org.example.studyplanner;
 
 import java.text.MessageFormat;
 
-public class ToDo implements PlannerMaterial{
+public class ToDo implements PlannerMaterial {
     private Integer id;
     private String title;
     private String description;
@@ -16,7 +16,7 @@ public class ToDo implements PlannerMaterial{
     }
 
     @Override
-    public String toString( ) {
+    public String toString() {
         return MessageFormat.format("[(Priority:{3}) ToDo {0}: {1}, {2}]", id, title, description, priority);
     }
 
@@ -50,5 +50,32 @@ public class ToDo implements PlannerMaterial{
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    // ---------- NOVOS MÉTODOS PARA EVITAR DATA CLASS ----------
+
+    public boolean isHighPriority() {
+        return priority > 5;
+    }
+
+
+    public String getSummary() {
+        return MessageFormat.format("ToDo {0}: Priority {1}", title, priority);
+    }
+
+
+    public void changePriority(int delta) {
+        int newPriority = this.priority + delta;
+        if (newPriority < 1) newPriority = 1;
+        else if (newPriority > 10) newPriority = 10;
+        this.priority = newPriority;
+    }
+
+
+    public boolean containsKeyword(String keyword) {
+        if (keyword == null) return false;
+        String lower = keyword.toLowerCase();
+        return (title != null && title.toLowerCase().contains(lower))
+                || (description != null && description.toLowerCase().contains(lower));
     }
 }
